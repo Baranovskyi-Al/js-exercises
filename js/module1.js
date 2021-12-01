1816cd7f4719a19c3e23f8421addb31e3410aaab
Oleksandr Baranovskyi <89201309+Baranovskyi-Al@users.noreply.github.com>
2021-12-01 15:58:31 +0200

    Update module1.js

--- js/module1.js
+++ js/module1.js
@@ -1,3 +1,143 @@
 // ----------------------Module #1
 
 // ------------------Exercise #1
+
+// // Change code below this line
+// const productName = "Droid";
+// const pricePerItem = 2000;
+// // console.log(productName);
+// // 'Droid'
+// // console.log(pricePerItem);
+// // 2000
+
+// ------------------Exercise #2
+
+// let productName = "Droid";
+// let pricePerItem = 2000;
+// // Change code below this line
+// productName = "Repair droid";
+// pricePerItem = pricePerItem + 1500;
+
+// ------------------Exercise #3
+
+// // Change code below this line
+// const topSpeed = 160;
+// let distance = 617.54;
+// let login = "mango935";
+// const isOnline = true;
+// const isAdmin = false;
+
+// ------------------Exercise #4
+
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+// // Change code below this line
+// const totalPrice = pricePerItem * orderedQuantity;
+
+// ------------------Exercise #5
+
+// const productName = "Droid";
+// const pricePerItem = 3500;
+// // Change code below this line
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+
+// ------------------Exercise #6
+
+// // Change code below this line
+// const orderedQuantity = 6;
+// const pricePerDroid = 800;
+// const deliveryFee = 50;
+// const totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`
+
+// ------------------Exercise #7
+
+// // Change code below this line
+// function sayHi(){
+// console.log("Hello, this is my first function!")
+// }
+// sayHi();
+
+// ------------------Exercise #8
+
+// // Change code below this line
+// function add(a, b, c) {
+//   console.log(`Addition result equals ${a + b +c}`);
+//   // Change code above this line
+// }
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+// ------------------Exercise #9
+
+// function add(a, b, c) {
+//   // Change code below this line
+//   return a + b + c;
+//   // Change code above this line
+// }
+// add(2, 5, 8); // 15
+// console.log(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
+
+// ------------------Exercise #10
+
+// function makeMessage(name, price) {
+//   // Change code below this line
+//   const message = `You picked ${name}, price per item is ${price} credits`;
+//   // Change code above this line
+//   return message;
+// }
+
+// ------------------Exercise #11
+
+// ------------------Exercise #12
+
+// ------------------Exercise #13
+
+// ------------------Exercise #14
+
+// ------------------Exercise #15
+
+// ------------------Exercise #16
+
+// ------------------Exercise #17
+
+// ------------------Exercise #18
+
+// ------------------Exercise #19
+
+// ------------------Exercise #20
+
+// ------------------Exercise #21
+
+// ------------------Exercise #22
+
+// ------------------Exercise #23
+
+// ------------------Exercise #24
+
+// ------------------Exercise #25
+
+// ------------------Exercise #26
+
+// ------------------Exercise #27
+
+// ------------------Exercise #28
+
+// ------------------Exercise #29
+
+// ------------------Exercise #30
+
+// ------------------Exercise #31
+
+// ------------------Exercise #31
+
+// ------------------Exercise #33
+
+// ------------------Exercise #34
+
+// ------------------Exercise #35
+
+// ------------------Exercise #36
