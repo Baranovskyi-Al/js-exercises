a09194975ea189a47661f9cd0643d6d8df80fc0f
Oleksandr Baranovskyi <89201309+Baranovskyi-Al@users.noreply.github.com>
2021-12-01 17:03:02 +0200

    Update module1.js

--- js/module1.js
+++ js/module1.js
@@ -92,25 +92,137 @@
 
 // ------------------Exercise #11
 
+// function calculateTotalPrice (orderedQuantity, pricePerItem) {
+//   // Change code below this line
+//   const totalPrice = orderedQuantity * pricePerItem;
+//   // Change code above this line
+//   return totalPrice;
+// };
+
 // ------------------Exercise #12
+
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//   // Change code below this line
+//   const totalPrice = orderedQuantity * pricePerDroid + deliveryFee;
+//   const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+//   // Change code above this line
+//   return message;
+// }
 
 // ------------------Exercise #13
 
+// function isAdult(age) {
+//   // Change code below this line
+//   const passed = age >= 18;
+//   // Change code above this line
+//   return passed;
+// }
+
 // ------------------Exercise #14
+
+// function isValidPassword(password) {
+//   const SAVED_PASSWORD = "jqueryismyjam";
+//   // Change code below this line
+//   const isMatch = password === SAVED_PASSWORD;
+//   // Change code above this line
+//   return isMatch;
+// }
 
 // ------------------Exercise #15
 
+// function checkAge(age) {
+//   let message;
+//   if (age >= 18) {
+//     // Change this line
+//     message = "You are an adult";
+//   } else {
+//     message = "You are a minor";
+//   }
+//   return message;
+// }
+
 // ------------------Exercise #16
+
+// function checkStorage(available, ordered) {
+//   let message;
+//   // Change code below this line
+//   if (available < ordered) {
+//     message = "Not enough goods in stock!";
+//   } else {
+//     message = "Order is processed, our manager will contact you.";
+//   }
+//   // Change code above this line
+//   return message;
+// }
 
 // ------------------Exercise #17
 
+// let a = 5;
+// let b = 10;
+// let c = 15;
+// let d = 20;
+// // Change code below this line
+// a += 2;
+// b -= 4;
+// c *= 3;
+// d /= 10;
+
 // ------------------Exercise #18
+
+// function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+//   let message;
+//   let totalPrice = pricePerDroid * orderedQuantity;
+//   // Change code below this line
+//   if (totalPrice > customerCredits) {
+//     message = "Insufficient funds!";
+//   } else {
+//     message = `You ordered ${orderedQuantity} droids, you have ${
+//       customerCredits - totalPrice
+//     } credits left`;
+//   }
+//   // Change code above this line
+//   return message;
+// }
 
 // ------------------Exercise #19
 
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+//   let message;
+//   if (password === null) {
+//     // Change this line
+//     message = "Canceled by user!";
+//   } else if (password === ADMIN_PASSWORD) {
+//     // Change this line
+//     message = "Welcome!";
+//   } else {
+//     message = "Access denied, wrong password!";
+//   }
+//   return message;
+// }
+
 // ------------------Exercise #20
 
+// function checkStorage(available, ordered) {
+//   let message;
+//   // Change code below this line
+//   if (ordered === 0) {
+//     message = "There are no products in the order!";
+//   } else if (ordered > available) {
+//     message = "Your order is too large, there are not enough items in stock!";
+//   } else {
+//     message = "The order is accepted, our manager will contact you";
+//   }
+//   // Change code above this line
+//   return message;
+// }
+
 // ------------------Exercise #21
+
+// function isNumberInRange(start, end, number) {
+//   const isInRange = start <= number && end >= number; // Change this line
+//   return isInRange;
+// }
 
 // ------------------Exercise #22
 
