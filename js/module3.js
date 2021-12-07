d2163d375028a5860840cafe1b4596cf8e391366
Oleksandr Baranovskyi <89201309+Baranovskyi-Al@users.noreply.github.com>
2021-12-07 12:22:02 +0200

    Update module3.js

--- js/module3.js
+++ js/module3.js
@@ -253,13 +253,100 @@
 
 // ------------------Exercise #16
 
+// function countTotalSalary(salaries) {
+//   let totalSalary = 0;
+//   // Change code below this line
+//   const values = Object.values(salaries);
+//   for (const value of values) {
+//     totalSalary += value;
+//   }
+//   // Change code above this line
+//   return totalSalary;
+// }
+
 // ------------------Exercise #17
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+// }
 
 // ------------------Exercise #18
 
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   // Change code below this line
+//   let price = 0;
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       price = product.price;
+//     }
+//   }
+//   return price;
+//   // Change code above this line
+// }
 // ------------------Exercise #19
 
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+//   // Change code below this line
+//   values = [];
+//   for (const value of products) {
+//     if (propName === "name") {
+//       values.push(value.name);
+//     }
+//     if (propName === "quantity") {
+//       values.push(value.quantity);
+//     }
+//     if (propName === "price") {
+//       values.push(value.price);
+//     }
+//   }
+//   return values;
+//   // Change code above this line
+// }
+
 // ------------------Exercise #20
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function calculateTotalPrice(productName) {
+//   // Пиши код ниже этой строки
+//   let totalPrice = 0;
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       totalPrice = product.price * product.quantity;
+//     }
+//   }
+//   return totalPrice;
+//   // Пиши код выше этой строки
+// }
 
 // ------------------Exercise #21
 
