f54e2eb3182d990b9da0278a695ce279ae72f904
Oleksandr Baranovskyi <89201309+Baranovskyi-Al@users.noreply.github.com>
2021-12-12 23:46:41 +0200

    add module4.js

---
+++ js/module4.js
@@ -0,0 +1,71 @@
+// ----------------------Module #4
+
+// ------------------Exercise #1
+
+// function makePizza() {
+//   return "Your pizza is being prepared, please wait.";
+// }
+// // Change code below this line
+// const result = makePizza();
+// const pointer = makePizza;
+
+// ------------------Exercise #2
+
+function deliverPizza(pizzaName) {
+  return `Delivering ${pizzaName} pizza.`;
+}
+function makePizza(pizzaName) {
+  return `Pizza ${pizzaName} is being prepared, please wait...`;
+}
+// Chande code below this line
+function makeMessage(pizzaName, callback) {
+  callback(pizzaName);
+  return deliverPizza(pizzaName);
+}
+
+// ------------------Exercise #3
+// ------------------Exercise #4
+// ------------------Exercise #5
+// ------------------Exercise #6
+// ------------------Exercise #7
+// ------------------Exercise #8
+// ------------------Exercise #9
+// ------------------Exercise #10
+// ------------------Exercise #11
+// ------------------Exercise #12
+// ------------------Exercise #13
+// ------------------Exercise #14
+// ------------------Exercise #15
+// ------------------Exercise #16
+// ------------------Exercise #17
+// ------------------Exercise #18
+// ------------------Exercise #19
+// ------------------Exercise #20
+// ------------------Exercise #21
+// ------------------Exercise #22
+// ------------------Exercise #23
+// ------------------Exercise #24
+// ------------------Exercise #25
+// ------------------Exercise #26
+// ------------------Exercise #27
+// ------------------Exercise #28
+// ------------------Exercise #29
+// ------------------Exercise #30
+// ------------------Exercise #31
+// ------------------Exercise #32
+// ------------------Exercise #33
+// ------------------Exercise #34
+// ------------------Exercise #35
+// ------------------Exercise #36
+// ------------------Exercise #37
+// ------------------Exercise #38
+// ------------------Exercise #39
+// ------------------Exercise #40
+// ------------------Exercise #41
+// ------------------Exercise #42
+// ------------------Exercise #43
+// ------------------Exercise #44
+// ------------------Exercise #45
+// ------------------Exercise #46
+// ------------------Exercise #47
+// ------------------Exercise #48
