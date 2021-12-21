5b4e78cad6280ab6ec0d0d0d0ee9ba8430709370
Oleksandr Baranovskyi <89201309+Baranovskyi-Al@users.noreply.github.com>
2021-12-21 13:35:22 +0200

    Update module5.js

--- js/module5.js
+++ js/module5.js
@@ -77,12 +77,110 @@
 // };
 
 // ------------------Exercise #4
+
+// const parent = {
+//   name: "Stacey",
+//   surname: "Moore",
+//   age: 54,
+//   heritage: "Irish",
+// };
+// // Change code below this line
+// const child = Object.create(parent);
+// // Change code above this line
+// child.name = "Jason";
+// child.age = 27;
+
 // ------------------Exercise #5
+
+// const ancestor = {
+//   name: "Paul",
+//   age: 83,
+//   surname: "Dawson",
+//   heritage: "Irish",
+// };
+// // Change code below this line
+// const parent = Object.create(ancestor);
+// parent.name = "Stacey";
+// parent.surname = "Moore";
+// parent.age = 54;
+
+// const child = Object.create(parent);
+// child.name = "Jason";
+// child.age = 27;
+// // Change code above this line
+
 // ------------------Exercise #6
+
+// class Car {}
+// const lada = new Car();
+
 // ------------------Exercise #7
+
+// class Car {
+//   // Change code below this line
+//   constructor(brand, model, price) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   // Change code above this line
+// }
+
 // ------------------Exercise #8
+
+// class Car {
+//   // Change code below this line
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   // Change code above this line
+// }
+
 // ------------------Exercise #9
+
+// class Car {
+//   constructor({ brand, model, price }) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//   }
+//   // Change code below this line
+//   getPrice() {
+//     return this.price;
+//   }
+//   changePrice(newPrice) {
+//     this.price = newPrice;
+//   }
+//   // Change code above this line
+// }
+
 // ------------------Exercise #10
+
+// class Storage {
+// 	constructor (array) {
+//     this.items = array;
+//     }
+// 	getItems() {
+//     return this.items;
+//     }
+// 	addItem(item) {
+//     this.items.push(item);
+//     }
+
+//   	removeItem(itemToRemove) {
+//     this.items = this.items.filter(remove => !itemToRemove.includes(remove));
+//     }
+// }
+// // Change code above this line
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
 // ------------------Exercise #11
 // ------------------Exercise #12
 // ------------------Exercise #13
